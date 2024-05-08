b3545bf23ba33b67be63aa3d5b3cd734eb12cc7a
Cassie Breviu <46505951+cassiebreviu@users.noreply.github.com>
2024-05-08 09:55:02 -0500

    fix writer

--- src/api/api/agents/writer/writer.py
+++ src/api/api/agents/writer/writer.py
@@ -1,48 +1,42 @@
 import sys
 import json
 import os
-from promptflow.core import Flow, Prompty, AzureOpenAIModelConfiguration
+from promptflow.tracing import trace
+from promptflow.core import Prompty, AzureOpenAIModelConfiguration
+from promptflow.core import Flow
+from pathlib import Path
+folder = Path(__file__).parent.absolute().as_posix()
 
-from dotenv import load_dotenv
 
-load_dotenv()
+@trace
+def execute(context, feedback, instructions, research):
+  
+  # Load prompty with AzureOpenAIModelConfiguration override
+  configuration = AzureOpenAIModelConfiguration(
+      azure_deployment=os.getenv("AZURE_DEPLOYMENT_NAME"),
+      api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+      api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+      azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+  )
 
-def write(context, feedback, instructions, research = []):
-    loaded_prompty = Flow.load("writer.prompty")
-    result = loaded_prompty(
-        context=context,
-        feedback=feedback,
-        instructions=instructions,
-        research=research,
-    )
+  override_model = {
+      "configuration": configuration,
+      "parameters": {"max_tokens": 512}
+  }
+    # create path to prompty file
+  prompty_file = folder + "/writer.prompty"
+  loaded_prompty = Flow.load(prompty_file, model=override_model)
+  result = loaded_prompty(
+      context=context,
+      feedback=feedback,
+      instructions=instructions,
+      research=research,
+  )
+  return result
 
-    # Load prompty with AzureOpenAIModelConfiguration override
-    configuration = AzureOpenAIModelConfiguration(
-        azure_deployment=os.getenv("AZURE_DEPLOYMENT_NAME"),
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
-    )
-
-    override_model = {
-        "configuration": configuration,
-        "parameters": {"max_tokens": 512}
-    }
-
-    prompty_obj = Prompty.load(
-        "writer/writer.prompty", model=override_model)
-    
-    result = prompty_obj(
-        context=context,
-        feedback=feedback,
-        instructions=instructions,
-        research=research,
-    )
-
-    return result
 
 def process(writer):
-    # parse string this chracter --- , artcile and feedback
+    # parse string this chracter --- , article and feedback
     result = writer.split("---")
     article = str(result[0]).strip()
     if len(result) > 1:
@@ -51,25 +45,33 @@ def process(writer):
         feedback = "No Feedback"
 
     return {
-        "context": {
-            "article": article,
-            "feedback": feedback,
-        }
+        "article": article,
+        "feedback": feedback,
     }
+
+
+def write(context, feedback, instructions, research):
+    result = execute(
+        context=context,
+        feedback=feedback,
+        instructions=instructions,
+        research=research,
+    )
+    processed = process(result)
+    return processed
+
+
 if __name__ == "__main__":
     # get args from the user
-    # check if args if null default to a value
-    # if len(sys.argv) == 0:
-    #     context="I want to write an article about Satya Nadella and the beginnings of his career.",
-    #     feedback="No Feedback",
-    #     instructions="Can you help me find information about Satya Nadella's career and beginnings?"
-    #     research = []
-    # else:
-    context = sys.argv[1]
-    feedback = sys.argv[2]
-    instructions = sys.argv[3]
-    research = json.dumps(sys.argv[4])
-    result = write(
+    context = "Can you find the latest camping trends and what folks are doing in the winter?"
+    feedback = "Research Feedback:\nAdditional specifics on how each phase of his education directly influenced particular career decisions or leadership styles at Microsoft would enhance the narrative. Information on key projects or initiatives that Nadella led, correlating to his expertise gained from his various degrees, would add depth to the discussion on the interplay between his education and career milestones."
+    instructions = "Can you find the relevant information on both him as a person and what he studied and maybe some news articles?"
+    research = []
+    # context = sys.argv[1]
+    # feedback = sys.argv[2]
+    # instructions = sys.argv[3]
+    # research = json.dumps(sys.argv[4])
+    result = execute(
         context=str(context),
         feedback=str(feedback),
         instructions=str(instructions),
