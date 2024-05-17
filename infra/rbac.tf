e3f46a2642c52deaf6108bf535ea720e4a646897
Andre Dewes <andredewes@microsoft.com>
2024-05-16 21:35:02 -0300

    Adding roles for user principal

--- infra/rbac.tf
+++ infra/rbac.tf
@@ -15,6 +15,13 @@ resource "azurerm_role_assignment" "openai_backend" {
   skip_service_principal_aad_check = true
 }
 
+resource "azurerm_role_assignment" "openai_user" {
+  count                            = try(local.is_default_workspace ? 0 : 1, 0)
+  principal_id                     = var.principal_id
+  role_definition_name             = "Cognitive Services OpenAI User"
+  scope                            = azurerm_cognitive_account.cog.id
+}
+
 resource "azurerm_role_assignment" "aisearchdata_backend" {
   count                            = try(local.is_default_workspace ? 0 : 1, 0)
   principal_id                     = azurerm_user_assigned_identity.uai.principal_id
@@ -25,10 +32,9 @@ resource "azurerm_role_assignment" "aisearchdata_backend" {
 
 resource "azurerm_role_assignment" "aisearchdata_user" {
   count                            = try(local.is_default_workspace ? 0 : 1, 0)
-  principal_id                     = azurerm_user_assigned_identity.uai.principal_id
+  principal_id                     = var.principal_id
   role_definition_name             = "Search Index Data Contributor"
   scope                            = azurerm_search_service.search.id
-  skip_service_principal_aad_check = true
 }
 
 resource "azurerm_role_assignment" "aisearchservice_backend" {
@@ -37,4 +43,11 @@ resource "azurerm_role_assignment" "aisearchservice_backend" {
   role_definition_name             = "Search Service Contributor"
   scope                            = azurerm_search_service.search.id
   skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "aisearchservice_user" {
+  count                            = try(local.is_default_workspace ? 0 : 1, 0)
+  principal_id                     = var.principal_id
+  role_definition_name             = "Search Service Contributor"
+  scope                            = azurerm_search_service.search.id
 }
