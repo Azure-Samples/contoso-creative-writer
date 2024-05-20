f3908a59a7063fb5a0ef7f02fc2ce8e7edaa4fbe
Cassie Breviu <46505951+cassiebreviu@users.noreply.github.com>
2024-05-20 06:14:27 -0700

    fix path

--- infra/hooks/postprovision.sh
+++ infra/hooks/postprovision.sh
@@ -30,8 +30,8 @@ echo ""
 echo "--- ✅ | 1. Post-provisioning - env configured ---"
 
 # Setup to run notebooks
-echo 'Installing dependencies from "./src/api/requirements.txt"'
-python -m pip install -r requirements.txt > /dev/null
+echo 'Installing dependencies from "src/api/requirements.txt"'
+python -m pip install -r src/api/requirements.txt > /dev/null
 python -m pip install ipython ipykernel > /dev/null      # Install ipython and ipykernel
 ipython kernel install --name=python3 --user > /dev/null # Configure the IPython kernel
 jupyter kernelspec list > /dev/null                      # Verify kernelspec list isn't empty
