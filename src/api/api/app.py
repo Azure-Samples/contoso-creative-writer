f9b9eb99cf09a1bf4a98270cd18a08632eca5847
Cedric Vidal <cedric@vidal.biz>
2024-07-17 21:38:09 +0000

    Python app can now also serve UI static files

--- src/api/api/app.py
+++ src/api/api/app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from os import getenv
 load_dotenv()
 
 # Instrument flask HTTP calls
@@ -9,9 +10,20 @@ from flask import Flask
 import api.get_article as get_article
 from api.logging import init_logging
 
-app = Flask(__name__)
-app.register_blueprint(get_article.bp)
+# Where to mount the API, defaults to root
+url_prefix = getenv("API_PREFIX")
+
+# Where are we serving the UI static files from? Defaults to not serving them.
+ui_folder = getenv("UI_FOLDER")
+
+app = Flask(__name__, static_url_path='', static_folder=ui_folder)
+app.register_blueprint(get_article.bp, url_prefix=url_prefix)
 init_logging(sampling_rate=1.0)
+
+if ui_folder:
+    @app.route('/')
+    def hello_world():
+        return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
