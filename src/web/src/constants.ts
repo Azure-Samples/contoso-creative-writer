4504a7aac1e6263083a7d06df106fbcbb9e3a093
Cedric Vidal <cedric@vidal.biz>
2024-07-18 19:37:43 +0000

    Support for local Codespaces dev workflow
    - Added instruction in README to set 8080 port to public
    - Added support to resolve the API's domain when on `github.dev` domain.

--- src/web/src/constants.ts
+++ src/web/src/constants.ts
@@ -1,4 +1,13 @@
+import { githubDevSubsPort } from "./utils/ghutils";
+
 const hostname = window.location.hostname;
-const apiEndpoint = (hostname === 'localhost' || hostname === '127.0.0.1') ? "http://localhost:8080/get_article" : "api/get_article";
+const apiPort = 8080
+
+
+const apiEndpoint = (hostname === 'localhost' || hostname === '127.0.0.1')
+    ? `http://localhost:${apiPort}/get_article`
+    : hostname.endsWith('github.dev') 
+    ? `${githubDevSubsPort(hostname, apiPort)}/get_article`
+    : "api/get_article";
 
 export { apiEndpoint };
