07fd758ac3abf727f04c574308abe1ed5ae31ea6
Cedric Vidal <cedric@vidal.biz>
2024-07-24 18:51:17 +0000

    Some logging to troubleshoot AppInsights

--- src/api/api/logging.py
+++ src/api/api/logging.py
@@ -16,19 +16,24 @@ def log_output(*args):
     logging.log(DEFAULT_LOG_LEVEL, *args)
 
 def init_logging(sampling_rate=1.0, log_level=DEFAULT_LOG_LEVEL):
+    logging.basicConfig(
+        level=log_level, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
     # Enable logging to app insights if a connection string is provided
     if 'APPINSIGHTS_CONNECTIONSTRING' in os.environ:
         connection_string=os.environ['APPINSIGHTS_CONNECTIONSTRING']
         if connection_string != "":
+            log_output(f"AppInsights connection string = {connection_string}")
             inject_openai_api()
             trace.set_tracer_provider(TracerProvider(sampler=ParentBasedTraceIdRatio(sampling_rate)))
             trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(AzureMonitorTraceExporter(connection_string=connection_string)))
+            log_output("AppInsights configured")
+    else:
+        log_output("AppInsights not configured")
 
     # Enable logging locally if the below variable is set
     if 'PROMPTFLOW_TRACING_SERVER' in os.environ and os.environ['PROMPTFLOW_TRACING_SERVER'] != 'false':
         start_pf_tracing()
 
-    logging.basicConfig(
-        level=log_level, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
     log_output("Logging initialized.")
