200a8fbb9673dc39edb74dfbdf8a60df5371c2a1
Marlene <57748216+marlenezw@users.noreply.github.com>
2024-08-21 12:27:20 +0000

    ui updates.

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -6,20 +6,39 @@ import Task from './components/task';
 
 function App() {
   return (
-    <main className="p-12">
-      
-      <div className="text-center text-3xl mt-8">Welcome to the Contoso Creative Writer</div>
-      <br></br>
-      <hr className="border-2 border-zinc-300" />
-      <br></br>
-      <span className="text-center text-1xl mt-8 ">This tool will help you write articles for the Contoso blog. Enter the required information and then click "Start Work". To watch the steps in the agent workflow complete, select the debug button in the button corner of the screen. The result will begin writing once the agent completed the tasks to write the article.</span>
-      <br></br>
-      <Task />
-      <hr className="border-2 border-zinc-300" />
-      <div className="text-center text-2xl mt-8">Article Result</div>
-      <Article />
-      <Toolbar />
-      <div className="fixed right-0 bottom-0 mr-12 mb-2 text-zinc-300">
+    <main className="p-8 flex flex-col min-h-screen">
+      {/* Header Section */}
+      <header className="text-center my-6">
+        <h1 className="text-4xl font-bold text-gray-800">Contoso Creative Writer</h1>
+        <p className="text-xl text-gray-600 mt-2">
+          This tool helps you write articles for the Contoso blog.
+          
+        </p>
+        <hr className="border-2 border-gray-300 my-6" />
+      </header>
+
+      {/* Main Content Wrapper */}
+      <div className="flex flex-col lg:flex-row lg:space-x-8 mt-8">
+        {/* Task Section - Left Aligned */}
+        <div className="lg:w-1/3 bg-gray-100 p-6 rounded shadow-md">
+          <h3 className="text-2xl text-gray-800 mb-4">Create Your Article</h3>
+          <Task />
+          <div className="text-center mt-4">
+            <Toolbar />
+          </div>
+        </div>
+
+        {/* Article Section - Right Aligned */}
+        <section className="lg:w-2/3 flex-grow mt-8 lg:mt-0">
+          <div className="bg-white shadow-md rounded p-6">
+            <h2 className="text-3xl text-gray-800 mb-4">Your Article</h2>
+            <Article />
+          </div>
+        </section>
+      </div>
+
+      {/* Version Number */}
+      <div className="fixed right-0 bottom-0 mr-6 mb-2 text-gray-400">
         {version}
       </div>
     </main>
@@ -27,3 +46,5 @@ function App() {
 }
 
 export default App;
+
+
