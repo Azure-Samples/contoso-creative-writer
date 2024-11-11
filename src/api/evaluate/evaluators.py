786bf76b89e1e05f070626ac2e2f711a61b54dea
Marlene <57748216+marlenezw@users.noreply.github.com>
2024-11-11 12:44:38 +0000

    Update evaluators.py

--- src/api/evaluate/evaluators.py
+++ src/api/evaluate/evaluators.py
@@ -1,47 +1,126 @@
 import os
 import json
 import logging
-
+import prompty
 from opentelemetry import trace
 from opentelemetry.trace import set_span_in_context
 from azure.ai.evaluation import RelevanceEvaluator, GroundednessEvaluator, FluencyEvaluator, CoherenceEvaluator, ContentSafetyEvaluator
 from azure.ai.evaluation import ViolenceEvaluator, HateUnfairnessEvaluator, SelfHarmEvaluator, SexualEvaluator
+from azure.ai.evaluation import evaluate
 
 from azure.identity import DefaultAzureCredential
 
 logging.getLogger('promptflow.core._prompty_utils').setLevel(logging.CRITICAL)
 
+class FriendlinessEvaluator:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, response):
+        model_config = {
+            "azure_deployment": os.environ.get("AZURE_OPENAI_4_EVAL_DEPLOYMENT_NAME"),
+            "azure_endpoint": os.environ.get("AZURE_OPENAI_ENDPOINT")
+        }
+        result = prompty.execute(
+            "friendliness.prompty",
+            configuration=model_config,
+            inputs={
+                "response": response,
+            }
+        )
+        return {"score": result}
 
 class ArticleEvaluator:
     def __init__(self, model_config, project_scope):
-        self.evaluators = [
-            RelevanceEvaluator(model_config),
-            FluencyEvaluator(model_config),
-            CoherenceEvaluator(model_config),
-            GroundednessEvaluator(model_config),
-            ViolenceEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
-            HateUnfairnessEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
-            SelfHarmEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
-            SexualEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential())
-        ]
+        self.evaluators = {
+            "relevance": RelevanceEvaluator(model_config),
+            "fluency": FluencyEvaluator(model_config),
+            "coherence": CoherenceEvaluator(model_config),
+            "groundedness": GroundednessEvaluator(model_config),
+            "violence": ViolenceEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
+            "hate-unfairness": HateUnfairnessEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
+            "self-harm": SelfHarmEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
+            "sexual": SexualEvaluator(azure_ai_project=project_scope, credential=DefaultAzureCredential()),
+            "friendliness": FriendlinessEvaluator(),
+        }
+        self.project_scope = project_scope
 
-    def __call__(self, *, query: str, context: str, response: str, **kwargs):
+    def __call__(self, *, data_path, **kwargs):
         output = {}
-        for evaluator in self.evaluators:
-            result = evaluator(
-                query=query,
-                context=context,
-                response=response,
-            )
-            output.update(result)
-
-            if not isinstance(evaluator, ContentSafetyEvaluator):
-                print(f"{evaluator} evaluation done!")
-            else:
-                print(f"Content saftey evaluation in done!")
-                
+        ## NOTE: - The following code expects that the user has Storage Blob Data Contributor permissions in order for the results to upload to the Azure AI Studio.
+        result = evaluate(
+            data=data_path,
+            evaluators=self.evaluators,
+            ## NOTE: If you do not have Storage Blob Data Contributor permissions, please comment out the below line of code. 
+            azure_ai_project=self.project_scope,
+            evaluator_config={
+                "relevance": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "fluency": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "coherence": {
+                        "column_mapping": {
+                        "response": "${data.response}",
+                        "query": "${data.query}",
+                    },
+                },
+                "groundedness": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "violence": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "self-harm": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "hate-unfairness": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "sexual": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+                "friendliness": {
+                    "column_mapping": {
+                        "response": "${data.response}",
+                        "context": "${data.context}",
+                        "query": "${data.query}",
+                    },
+                },
+            },
+        )
+        output.update(result)
         return output
-
+    
 def evaluate_article(data, trace_context):
     tracer = trace.get_tracer(__name__)
     with tracer.start_as_current_span("run_evaluators", context=trace_context) as span:
@@ -57,7 +136,7 @@ def evaluate_article(data, trace_context):
             "project_name": os.environ["AZURE_AI_PROJECT_NAME"],        
         }
         evaluator = ArticleEvaluator(configuration, project_scope)
-        results = evaluator(query=data['query'], context=data['context'], response=data['response'])
+        results = evaluator(data)
         resultsJson = json.dumps(results)
         span.set_attribute("output", resultsJson)
 
