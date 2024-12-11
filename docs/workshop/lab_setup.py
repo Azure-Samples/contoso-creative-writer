8c39b6d865b9f9ef4abc8703efb35933910be9b7
Cedric Vidal <cedric@vidal.biz>
2024-12-11 21:20:24 +0000

    git fetch origin

--- docs/workshop/lab_setup.py
+++ docs/workshop/lab_setup.py
@@ -74,6 +74,15 @@ def fork_repository():
     out, err = process.communicate()
     print(out)
 
+@step("Fetch Origin")
+def fetch_origin():
+    """
+    Fetch the latest changes from origin.
+
+    Necessary because after forking repository, main is still tracking upstream and origin has not been fetched.
+    """
+    subprocess.run(['git', 'fetch', 'origin'], check=True)
+
 @step("Azure CLI Authentication")
 def azure_login(*, username: str = None, password: str = None, tenant: str = None, force: bool = False):
     # Only check authentication status if not forcing re-auth
