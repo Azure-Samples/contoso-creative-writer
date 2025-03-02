ed40ce2ba512fd724221324ea025e4292e9fd978
Fethi Dilmi <fethidilmi@microsoft.com>
2025-03-02 13:02:45 +0100

    fix: plug workshop scripts to grounding with bing api

--- docs/workshop/socialmedia/social.py
+++ docs/workshop/socialmedia/social.py
@@ -5,7 +5,7 @@ import prompty.azure
 from prompty.tracer import trace, Tracer, PromptyTracer
 
 # Import the researcher agent to use here
-sys.path.append(os.path.abspath('../../docs/workshop/researcher/'))
+sys.path.append(os.path.abspath('../researcher/'))
 from researcher3 import research
 
 #initiate local prompty tracing
@@ -22,13 +22,13 @@ def execute_social_media_writer_prompty(research_context: str, research, social_
 
     return reseponse
 
-def run_social_media_agent(instructions: str, social_media_instructions: str):
+def run_social_media_agent(instructions: str, social_media_instructions: str, research_prompty_file_path='../researcher/researcher-2.prompty'):
     """
     Run the social media agent
 
     Execute the researcher prompty to find information, entities, and news
     execute the social media writer prompty to create the twitter thread
     """
-    research_results = research(instructions)
+    research_results = research(instructions, prompty_file_path=os.path.abspath(research_prompty_file_path))
     thread = execute_social_media_writer_prompty(research_context= instructions, research=research_results, social_media_instructions = social_media_instructions)
     print(thread)
