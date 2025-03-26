049865b99fc53027a6fb9832cfedcea770af29b4
Cedric Vidal <cedric@vidal.biz>
2025-03-26 11:44:17 +0900

    Removed commented code

--- src/api/orchestrator.py
+++ src/api/orchestrator.py
@@ -8,7 +8,6 @@ import json
 from agents.researcher import researcher
 from agents.product import product
 from agents.writer import writer
-# from agents.designer import designer
 from agents.editor import editor
 from evaluate.evaluators import evaluate_article_in_background
 from prompty.tracer import trace, Tracer, console_tracer, PromptyTracer
@@ -95,11 +94,6 @@ def create(research_context, product_context, assignment_context, evaluate=False
         yield complete_message("partial", {"text": item})
 
     processed_writer_result = writer.process(full_result)
-
-    # send article to the designer, to generate an image for the blog
-    # yield start_message("designer")
-    # designer_response = designer.design(processed_writer_result['article'])
-    # yield complete_message("designer", [f"Image stored in {designer_response}"])
 
     # Then send it to the editor, to decide if it's good or not
     yield start_message("editor")
